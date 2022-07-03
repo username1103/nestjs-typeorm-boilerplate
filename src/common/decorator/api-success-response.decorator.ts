16da80f2ee1a262a797bf159f75fcc5a635a2813
myeongil <username1103@naver.com>
2022-07-04 02:39:46 +0900

    Feat: Add Swagger

---
+++ src/common/decorator/api-success-response.decorator.ts
@@ -0,0 +1,15 @@
+import { applyDecorators, HttpCode, HttpStatus, Type } from '@nestjs/common';
+import { ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { ResponseEntity } from '../response/response-entity';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function ApiSuccessResponse(status: HttpStatus, data?: Type<unknown> | Function | [Function] | string) {
+  class Temp extends ResponseEntity<any> {
+    @ApiProperty({ type: data })
+    get data() {
+      return super.data;
+    }
+  }
+
+  return applyDecorators(HttpCode(status), ApiResponse({ type: data && Temp, status, description: HttpStatus[status] }));
+}
--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppConfigService } from './common/config/app/config.service';
 import { setNestApp } from './setNsetApp';
+import { setSwagger } from './setSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   setNestApp(app);
 
+  setSwagger(app);
+
   const appConfigService = app.get(AppConfigService);
   await app.listen(appConfigService.port);
 }
+
 bootstrap();
